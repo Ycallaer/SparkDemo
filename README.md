da3852c453ca399a50d9655f2a1f98b46e68c422
yvescallaert <yves.callaert@maersk.com>
2018-01-02 16:12:10 +0100

    changing part of the code in the original exercise

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SparkDemo
-Scala demo for spark 2.x
+The exercises are coming from the edx course: DAT202.3x
+
+Instead of using azure as a source (and having to go to the trouble of setting up azure infra) to do data analytics I'd rather work on my local machine.
+
+So I've set up a local spark cluster on my machine and adapting the code where necessary so it can run from Intellij directly towards my local spark cluster
+
+
--- src/main/scala/Lab1.scala
+++ src/main/scala/Lab1.scala
@@ -27,6 +27,25 @@ object DemoScala {
     val flightsByOrigin= flights.join(airports,flights.col("OriginAirportID") === airports.col("airport_id")).groupBy("city").count()
     flightsByOrigin.show()
 
+    //Determine Summary Statistics¶
+    //Predictive modeling is based on statistics and probability, so you will often start by looking at summary statistics. The describe function returns a DataFrame containing the count, mean, standard deviation, minimum, and maximum values for each numeric column.
+
+    flights.describe().show()
+
+    //Determine the Presence of Duplicates
+    //The data you have to work with won't always be perfect - often you'll want to clean the data; for example to detect and remove duplicates that might affect your model. You can use the dropDuplicates function to create a new DataFrame with the duplicates removed, enabling you to determine how many rows are duplicates of other rows.
+
+    flights.count() - flights.dropDuplicates().count()
+
+    //Identify Missing Values¶
+    //As well as determing if duplicates exist in your data, you should detect missing values, and either remove rows containing missing data or replace the missing values with a suitable relacement. The na.drop function creates a DataFrame with any rows containing missing data removed - you can specify a subset of columns, and whether the row should be removed in any or all values are missing. You can then use this new DataFrame to determine how many rows contain missing values.
+    flights.count() - flights.dropDuplicates().na.drop("any", Array("ArrDelay", "DepDelay")).count()
+
+    //Clean the Data¶
+    //Now that you've identified that there are duplicates and missing values, you can clean the data by removing the duplicates and replacing the missing values. The na.fill function replaces missing values with a specified replacement value. In this case, you'll remove all duplicate rows and replace missing ArrDelay and DepDelay values with 0.
+
+    val data=flights.dropDuplicates().na.fill(0, Array("ArrDelay", "DepDelay"))
+    data.count()
 
   }
 }
